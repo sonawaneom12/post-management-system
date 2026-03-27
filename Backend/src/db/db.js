224b4d3a7250390cf88d75bf564c472330177760
Om Sonawane <111688696+sonawaneom12@users.noreply.github.com>
2026-03-27 21:12:21 +0530

    Update MongoDB connection string placeholder

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 async function connectDB() {
-    await mongoose.connect("mongodb+srv://new_user_1:lYX7Y5TaXVSU7ErU@backend-tutorials.6zrmjyd.mongodb.net/project-1");
+    await mongoose.connect("YOUR MONGO_DB ATLAS CLUSTER URL");
 
     console.log("DB Connected");
 }
